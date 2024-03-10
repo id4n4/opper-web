5d3b0a202e3fa1898c91ae5de531d6b3fb3d292d
david sanchez <nagatiitoo@hotmail.com>
2024-03-10 15:02:32 -0500

    Agrega páginas y constantes de rutas

---
+++ src/constants/paths.ts
@@ -0,0 +1,9 @@
+export const PATH = {
+  HOME: "/",
+  ABOUT: "/about",
+  CONTACT: "/contact",
+  MODELING_TOOLS: "/modeling-tools",
+  OPPER_APPS: "/opper-apps",
+  SERVICES: "/services",
+  TG: "/tg",
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,19 +2,20 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#00cc99",
-      secondary: "#72757e",
-      tertiary: "#bfbab7",
-      background: "#16161a",
-      headline: "#fffffe",
-      paragraph: "#94a1b2",
-      button: "#00cc99",
-      buttonText: "#fffffe",
-      stroke: "#010101",
-      main: "#fffffe",
-      cardBackground: "#242629",
+    extend: {
+      colors: {
+        primary: "#00cc99",
+        secondary: "#72757e",
+        tertiary: "#bfbab7",
+        background: "#16161a",
+        headline: "#fffffe",
+        paragraph: "#94a1b2",
+        button: "#00cc99",
+        buttonText: "#fffffe",
+        stroke: "#010101",
+        main: "#fffffe",
+        cardBackground: "#242629",
+      },
     },
   },
   plugins: [],
