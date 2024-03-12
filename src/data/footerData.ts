73219a97e36a27c1c8890d81a8a375f347cb417b
david sanchez <nagatiitoo@hotmail.com>
2024-03-11 22:52:42 -0500

    Agrega configuración de VSCode, paquete react-icons y componente MainFooter.

---
+++ src/data/footerData.ts
@@ -0,0 +1,24 @@
+import { PATH } from "@/constants/paths"
+
+export const footerText = {
+  opperDescription: 'Nace para brindar al mercado Colombiano soporte tecnológico y de consultoría para la toma de decisiones a nivel estratégico, táctico y operacional.',
+  opperAddress: 'Cra 13B # 61 B - 08 ',
+  opperNeighborhood: 'La castellana',
+  opperCity: 'Montería',
+  opperState: 'Córdoba',
+  opperCountry: 'Colombia',
+  opperPhone: '+57 4 448 90 90',
+  opperEmail: 'indugarciaperez@gmail.com'
+
+}
+
+export const footerLinks = {
+  instagram: 'http://www.instagram.com/opper_sas'
+}
+
+export const tools = [
+  {id: 1, name: 'Opper Apps', href: PATH.OPPER_APPS},
+  {id: 2, name: 'TG', href: PATH.TG},
+  {id: 3, name: 'Herramientas de modelación', href: PATH.MODELING_TOOLS},
+  {id: 4, name: 'Servicios', href: PATH.SERVICES},
+]
