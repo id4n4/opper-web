0604e97bc5c518e38498d7a64592559c97a44fbb
david sanchez <nagatiitoo@hotmail.com>
2024-03-27 09:00:43 -0500

    Actualizar URL del sitio en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/opper-page',
+  site: 'https://opper.com.co',
   integrations: [tailwind(), react()],
 });
