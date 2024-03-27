97f21ca16ab7043505074583b248ac05260bc90e
david sanchez <nagatiitoo@hotmail.com>
2024-03-27 10:50:29 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://id4n4.github.io',
-  base: 'opper-page',
   integrations: [tailwind(), react()],
 });
